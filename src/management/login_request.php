fbb42c2aa5d9322f5b35d905912466c11a44aa4f
ndaniel <132856298+DannyBrando@users.noreply.github.com>
2026-01-23 09:12:52 +0100

    Récupération des inputs du form

--- src/management/login_request.php
+++ src/management/login_request.php
@@ -1,27 +1,17 @@
 <?php
-require_once '../pages/db.php';
+$name = $email = $password = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // 1. Récupérer les données du formulaire
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    try {
-        $sql = "INSERT INTO items (username, email, password) VALUES (:nom, :prix, :description)";
-        $stmt = $pdo->prepare($sql);
-
-        $stmt->execute([
-            ':nom' => $nom,
-            ':prix' => $prix,
-            ':description' => $desc
-        ]);
-
-        echo "Le produit a été ajouté avec succès !";
-        echo "<a href='main.php'>Voir la liste des produits</a>";
-
-    } catch (PDOException $e) {
-        echo "Erreur lors de l'ajout : " . $e->getMessage();
-    }
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = process_input($_POST["username"]);
+    $password = process_input($_POST["password"]);
+    $email = process_input($_POST["email"]);
 }
+
+function process_input($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 ?>
